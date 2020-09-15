f53657a12cad0ba1f5c3f58e3d53b74e5de97407
Pete Schaaf <peteschaaf.19@gmail.com>
2020-09-15 13:50:07 -0400

    FizzBuzz interpret method finished, first iteration

--- src/main/java/project/FizzBuzz.java
+++ src/main/java/project/FizzBuzz.java
@@ -2,6 +2,15 @@ package project;
 
 public class FizzBuzz {
     public String interpret(int numberToSay) {
-        return "1";
+        if (numberToSay % 3 == 0 && numberToSay % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (numberToSay % 3 == 0) {
+            return "Fizz";
+        }
+        if (numberToSay % 5 == 0) {
+            return "Buzz";
+        }
+        return "" + numberToSay;
     }
 }
--- src/test/java/project/FizzBuzzTest.java
+++ src/test/java/project/FizzBuzzTest.java
@@ -26,7 +26,66 @@ public class FizzBuzzTest {
 
         //Assertion
         assertEquals("1", result);
+    }
+    @Test
+    public void given2ShouldReturn2(){
+        FizzBuzz underTest = new FizzBuzz();
 
+        String result = underTest.interpret(2);
+
+        assertEquals("2", result);
+    }
+    @Test
+    public void given3ShouldReturnFizz() {
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(3);
+
+        assertEquals("Fizz", result);
+    }
+    @Test
+    public void given5ShouldReturnBuzz() {
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(5);
+
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void given6ShouldReturnFizz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(6);
+
+        assertEquals("Fizz", result);
+    }
+    @Test
+    public void given10ShouldReturnBuzz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(10);
+
+        assertEquals("Buzz", result);
 
     }
+    @Test
+    public void given15ShouldReturnFizzBuzz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(15);
+
+        assertEquals("FizzBuzz", result);
+
+    }
+    @Test
+    public void given30ShouldReturnFizzBuzz(){
+        FizzBuzz underTest = new FizzBuzz();
+
+        String result = underTest.interpret(30);
+
+        assertEquals("FizzBuzz", result);
+
+    }
+
 }
